295e9a192943ca0ef415e253a99a3aa7fefd1670
Kauan Lazzarin <kauanlazzarin@gmail.com>
2021-04-09 19:28:44 -0400

    responsividade do about melhorada e componentes icon hover adicionados

--- src/components/About-Me/AboutMe.jsx
+++ src/components/About-Me/AboutMe.jsx
@@ -10,7 +10,7 @@ export default function AboutMe () {
             <main className={styles.aboutCardBody}>
                 
 
-                <div>
+                <div className={styles.cardInfo}>
                     <img src="/assets/me.jpeg" />
                     <p>
                         I've started studying web development on January 2020,
@@ -18,16 +18,53 @@ export default function AboutMe () {
                         my skills. <br/>
                         I'm 17 and I current live in Ouro Preto d'Oeste, Rondônia, Brazil
                     </p>
-
-
                 </div>
 
-                <div>
-                    <a href=""><img src="/icons/discord-icon.svg" alt=""/></a>
-                    <a href="https://github.com/KauanLazzarin"><img src="/icons/github-icon.svg" alt=""/></a>
-                    <a href=""><img src="/icons/gmail-icon.svg" alt=""/></a>
-                    <a href="https://www.instagram.com/kauan.lzz/"><img src="/icons/instagram-icon.svg" alt=""/></a>
-                    <a href="https://www.linkedin.com/in/kauan-lazzarin-6804a11b4/"><img src="/icons/linkedin-icon.svg" alt=""/></a>
+                <div className="no-desktop" id="mobileIcons">   
+                    <div className={styles.mobileIcons}>
+                        <a className={styles.mobileIcon} href=""><img src="/icons/discord-icon.svg" alt=""/></a>
+                        <a className={styles.mobileIcon} href="https://github.com/KauanLazzarin"><img src="/icons/github-icon.svg" alt=""/></a>
+                        <a className={styles.mobileIcon} href=""><img src="/icons/gmail-icon.svg" alt=""/></a>
+                        <a className={styles.mobileIcon} href="https://www.instagram.com/kauan.lzz/"><img src="/icons/instagram-icon.svg" alt=""/></a>
+                        <a className={styles.mobileIcon} href="https://www.linkedin.com/in/kauan-lazzarin-6804a11b4/"><img src="/icons/linkedin-icon.svg" alt=""/></a>
+                    </div>
+                </div>
+
+                <div className="no-mobile hoverIconsContainer">
+                    <div className={styles.desktopHoverIcon}>
+                        <img src="/icons/discord-icon.svg" alt=""/>
+                        <div className={styles.iconInfo}>
+                            <strong>Kauan Lazzarin#9685</strong>
+                        </div>
+                    </div>
+
+                    <div className={styles.desktopHoverIcon}>
+                        <img src="/icons/github-icon.svg" alt=""/>
+                        <div className={styles.iconInfo}>
+                            <a href="https://github.com/KauanLazzarin">Github</a>
+                        </div>
+                    </div>
+
+                    <div className={styles.desktopHoverIcon}>
+                        <img src="/icons/gmail-icon.svg" alt=""/>
+                        <div className={styles.iconInfo}>
+                            <strong>kauanlazzarin@gmail.com</strong>
+                        </div>
+                    </div>
+
+                    <div className={styles.desktopHoverIcon}>
+                        <img src="/icons/instagram-icon.svg" alt=""/>
+                        <div className={styles.iconInfo}>
+                            <a href="https://www.instagram.com/kauan.lzz/">Instagram</a>
+                        </div>
+                    </div>
+
+                    <div className={styles.desktopHoverIcon}>
+                        <img src="/icons/linkedin-icon.svg" alt=""/>
+                        <div className={styles.iconInfo}>
+                            <a href="https://www.linkedin.com/in/kauan-lazzarin-6804a11b4/">Linkedin</a>
+                        </div>
+                    </div>
                 </div>
             </main>
         </>
